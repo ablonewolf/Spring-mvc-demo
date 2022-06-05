aaa8a0f3aea07a212e5214a817a368130c5cbf82
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2022-06-06 05:36:20 +0600

    adding all the files

---
+++ src/main/java/com/arka99/Springmvcdemo/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.arka99.Springmvcdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
